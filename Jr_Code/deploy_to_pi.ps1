b8115bcca73cedad88b8d03d8cb1df8c02c06cc8
Md.Kutub Al Baki <kutubalbaki007@gmail.com>
2026-04-24 23:51:54 +0600

    Add files via upload

---
+++ Jr_Code/deploy_to_pi.ps1
@@ -0,0 +1,9 @@
+﻿param([string]$PiHost = "raspberrypi.local", [string]$PiUser = "pi")
+
+Write-Host "Deploying to $PiUser@$PiHost..." -ForegroundColor Cyan
+
+ssh "$PiUser@$PiHost" "mkdir -p ~/mariner/pi_scripts"
+scp -r .\pi_scripts\* "$PiUser@$PiHost`:~/mariner/pi_scripts/"
+ssh "$PiUser@$PiHost" "chmod +x ~/mariner/pi_scripts/*.sh"
+
+Write-Host "Done! Run: ssh $PiUser@$PiHost 'cd ~/mariner/pi_scripts && ./start_all_services.sh'" -ForegroundColor Green
